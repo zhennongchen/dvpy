68c515ac8e146289b62f27dc01b9fe826710d0f0
Zhennong Chen <chenzhennong@gmail.com>
2019-01-17 12:18:50 -0800

    change learning rate

--- src/dvpy/tf/NumpyArrayIterator.py
+++ src/dvpy/tf/NumpyArrayIterator.py
@@ -72,7 +72,7 @@ class NumpyArrayIterator(IteratorBase):
         batch_y1= np.zeros(
             tuple([current_batch_size]) + self.shape + tuple([self.output_channels])
         )
-        batch_y2=np.zeros(tuple([current_batch_size])+(1,))
+        batch_y2=np.zeros(tuple([current_batch_size])+(3,))
         #batch_y3=np.zeros(tuple([current_batch_size])+(3,))
         #batch_y4=np.zeros(tuple([current_batch_size])+(3,))
         #batch_y4=np.zeros(tuple([current_batch_size])+(3,))
@@ -126,7 +126,7 @@ class NumpyArrayIterator(IteratorBase):
                 batch_x[i] = x
 
             batch_y1[i] = label
-            batch_y2[i] = translation_n[0]
+            batch_y2[i] = translation_n
            
             #batch_y3[i] = x_n
             #batch_y4[i] = y_n
