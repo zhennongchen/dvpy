fa4322351877c3312aa928dd7f047ba14bfced2b
Zhennong Chen <chenzhennong@gmail.com>
2019-05-02 16:51:11 -0700

    trial

--- src/dvpy/zc_random.py
+++ src/dvpy/zc_random.py
@@ -10,8 +10,9 @@ def zc_random(params, shape):
 
     translation = np.eye(params.image_dimension + 1)
     for t, ax in zip(params.translation_range, params.img_spatial_indices):
-        random_t=np.random.uniform(t,t)
-        translation[ax, params.image_dimension] =random_t #* shape[ax]
+        if ax == 1:
+            random_t=np.random.uniform(t,t)
+            translation[ax, params.image_dimension] =random_t #* shape[ax]
 
 
 
