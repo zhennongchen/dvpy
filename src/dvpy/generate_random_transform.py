6b922bce8a709efb8322084757fca332c20c8610
Zhennong Chen <chenzhennong@gmail.com>
2019-05-03 12:53:26 -0700

    print

--- src/dvpy/generate_random_transform.py
+++ src/dvpy/generate_random_transform.py
@@ -11,7 +11,7 @@ def generate_random_transform(params, shape):
     translation = np.eye(params.image_dimension + 1)
     #raw code   
     for t, ax in zip(params.translation_range, params.img_spatial_indices):
-        random_t=np.random.uniform(-t, 0)
+        random_t=np.random.uniform(-t, t)
         translation[ax, params.image_dimension] =random_t * shape[ax]
 
     #only translate to the first quadrant
--- src/dvpy/transform_matrix_offset_center.py
+++ src/dvpy/transform_matrix_offset_center.py
@@ -23,12 +23,16 @@ def transform_full_matrix_offset_center(matrix, shape):
         )
 
     shape = np.array([float(s) / 2.0 + 0.5 for s in shape])
-
+    print(shape)
     for_mat = np.eye(len(shape) + 1)
     rev_mat = np.eye(len(shape) + 1)
+    
 
     for_mat[:-1, -1] = +shape
     rev_mat[:-1, -1] = -shape
+    print(for_mat)
+    print(np.dot(for_mat, matrix))
+    print(np.dot(np.dot(for_mat, matrix), rev_mat))
 
     return np.dot(np.dot(for_mat, matrix), rev_mat)
 
