282138b69fd46f714eb8c3c50fb08d819089de64
Zhennong Chen <chenzhennong@gmail.com>
2019-05-03 13:04:09 -0700

    print

--- src/dvpy/transform_matrix_offset_center.py
+++ src/dvpy/transform_matrix_offset_center.py
@@ -26,12 +26,13 @@ def transform_full_matrix_offset_center(matrix, shape):
     print(shape)
     for_mat = np.eye(len(shape) + 1)
     rev_mat = np.eye(len(shape) + 1)
-    
+   
 
     for_mat[:-1, -1] = +shape
     rev_mat[:-1, -1] = -shape
+    print('matrix input',matrix)
     print(for_mat)
-    print(np.dot(for_mat, matrix))
+    print('multiply of for_mat and matrix',np.dot(for_mat, matrix))
     print(np.dot(np.dot(for_mat, matrix), rev_mat))
 
     return np.dot(np.dot(for_mat, matrix), rev_mat)
