9256c5a269bc492ae2c1ef6425169ed6b0805852
Zhennong Chen <chenzhennong@gmail.com>
2019-08-02 09:58:19 -0700

    just seg

--- src/dvpy/tf/NumpyArrayIterator.py
+++ src/dvpy/tf/NumpyArrayIterator.py
@@ -72,9 +72,9 @@ class NumpyArrayIterator(IteratorBase):
         batch_y1= np.zeros(
             tuple([current_batch_size]) + self.shape + tuple([self.output_channels])
         )
-        batch_y2=np.zeros(tuple([current_batch_size])+(3,))
-        batch_y3=np.zeros(tuple([current_batch_size])+(3,))
-        batch_y4=np.zeros(tuple([current_batch_size])+(3,))
+        #batch_y2=np.zeros(tuple([current_batch_size])+(3,))
+        #batch_y3=np.zeros(tuple([current_batch_size])+(3,))
+        #batch_y4=np.zeros(tuple([current_batch_size])+(3,))
      
 
         ##
@@ -142,9 +142,9 @@ class NumpyArrayIterator(IteratorBase):
                 batch_x[i] = x
 
             batch_y1[i] = label
-            batch_y2[i] = translation_n
-            batch_y3[i] = x_n
-            batch_y4[i] = y_n
+            #batch_y2[i] = translation_n
+            #batch_y3[i] = x_n
+            #batch_y4[i] = y_n
             
         ##
         ## Return
@@ -159,7 +159,7 @@ class NumpyArrayIterator(IteratorBase):
         outputs = {
             name: layer
             for name, layer in zip(
-                self.image_data_generator.output_layer_names, [batch_y1,batch_y2,batch_y3,batch_y4]
+                self.image_data_generator.output_layer_names, [batch_y1]
             )
         }
         
