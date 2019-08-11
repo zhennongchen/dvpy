aa1ef05bba45746316ff284596a57ed3d3183c5b
Zhennong Chen <chenzhennong@gmail.com>
2019-08-11 16:26:24 -0700

    retrain for x

--- src/dvpy/tf/NumpyArrayIterator.py
+++ src/dvpy/tf/NumpyArrayIterator.py
@@ -73,7 +73,7 @@ class NumpyArrayIterator(IteratorBase):
             tuple([current_batch_size]) + self.shape + tuple([self.output_channels])
         )
         batch_y2=np.zeros(tuple([current_batch_size])+(3,))
-        #batch_y3=np.zeros(tuple([current_batch_size])+(3,))
+        batch_y3=np.zeros(tuple([current_batch_size])+(3,))
         #batch_y4=np.zeros(tuple([current_batch_size])+(3,))
      
 
@@ -143,7 +143,7 @@ class NumpyArrayIterator(IteratorBase):
 
             batch_y1[i] = label
             batch_y2[i] = translation_n
-            #batch_y3[i] = x_n
+            batch_y3[i] = x_n
             #batch_y4[i] = y_n
             
         ##
@@ -159,7 +159,7 @@ class NumpyArrayIterator(IteratorBase):
         outputs = {
             name: layer
             for name, layer in zip(
-                self.image_data_generator.output_layer_names, [batch_y1,batch_y2]
+                self.image_data_generator.output_layer_names, [batch_y1,batch_y2,batch_y3]
             )
         }
         
