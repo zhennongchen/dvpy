c0ea307ed50d1de722e896c772229461f520b379
Zhennong Chen <chenzhennong@gmail.com>
2019-09-18 01:25:25 -0700

    check

--- src/dvpy/learning_rate_step_decay2.py
+++ src/dvpy/learning_rate_step_decay2.py
@@ -6,6 +6,7 @@ def learning_rate_step_decay2(epoch, lr, step=25, initial_power=-3):
     The learning rate begins at 10^initial_power,
     and decreases by a factor of 10 every `step` epochs.
     """
+    ##
     num = epoch // step
     lrate = 10 ** (initial_power - num)
     print("Learning rate plan 2for epoch {} is {}.".format(epoch + 1, 1.0 * lrate))
