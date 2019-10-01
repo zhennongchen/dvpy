63a0a77e2e87dc9d6c674104de3117673265d6bd
Zhennong Chen <chenzhennong@gmail.com>
2019-10-01 11:00:06 -0700

    LV length

--- src/dvpy/tf/NumpyArrayIterator.py
+++ src/dvpy/tf/NumpyArrayIterator.py
@@ -73,9 +73,9 @@ class NumpyArrayIterator(IteratorBase):
         batch_y1= np.zeros(
             tuple([current_batch_size]) + self.shape + tuple([self.output_channels])
         )
-        batch_y2=np.zeros(tuple([current_batch_size])+(3,))
-        batch_y3=np.zeros(tuple([current_batch_size])+(3,))
-        batch_y4=np.zeros(tuple([current_batch_size])+(3,))
+        batch_y2=np.zeros(tuple([current_batch_size])+(1,))
+        #batch_y3=np.zeros(tuple([current_batch_size])+(3,))
+        #batch_y4=np.zeros(tuple([current_batch_size])+(3,))
      
 
         ##
@@ -133,8 +133,8 @@ class NumpyArrayIterator(IteratorBase):
 
             batch_y1[i] = label
             batch_y2[i] = t_c_n
-            batch_y3[i] = x_n
-            batch_y4[i] = y_n
+            #batch_y3[i] = x_n
+            #batch_y4[i] = y_n
             
         ##
         ## Return
@@ -149,7 +149,7 @@ class NumpyArrayIterator(IteratorBase):
         outputs = {
             name: layer
             for name, layer in zip(
-                self.image_data_generator.output_layer_names, [batch_y1,batch_y2,batch_y3,batch_y4]
+                self.image_data_generator.output_layer_names, [batch_y1,batch_y2]
             )
         }
         
