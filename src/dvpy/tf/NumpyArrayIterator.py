a4b153e57b6e66f4eb99e33ffbd89d558c1a5f0e
Zhennong Chen <chenzhennong@gmail.com>
2019-10-26 12:20:02 -0700

    no print

--- src/dvpy/tf/NumpyArrayIterator.py
+++ src/dvpy/tf/NumpyArrayIterator.py
@@ -102,7 +102,7 @@ class NumpyArrayIterator(IteratorBase):
             #Retrieve the path to the matrix npy file (the original translation vector)
             patient_id = os.path.dirname(os.path.dirname(self.X[j]))
             affine_path = os.path.join(patient_id,'affine',self.view+'_new.npy')
-            print(affine_path)
+           
             M = np.load(affine_path,allow_pickle=True)
             pad_path = os.path.join(patient_id,'affine/padding_coordinate_conversion.npy')
             pad_v = np.load(pad_path,allow_pickle=True)
