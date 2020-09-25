6e31a4e6da8ce3595269ca27700b96302a615b2e
Zhennong Chen <chenzhennong@gmail.com>
2020-09-25 03:18:09 -0400

    np

--- src/dvpy/tf/NumpyArrayIterator.py
+++ src/dvpy/tf/NumpyArrayIterator.py
@@ -88,7 +88,6 @@ class NumpyArrayIterator(IteratorBase):
 
             # Retrieve the path to the input image...
             x = self.X[j]
-            print(x)
             # ...and convert the path to a raw (unnormalized) image.
             if self.input_adapter is not None:
                 x = self.input_adapter(x)
