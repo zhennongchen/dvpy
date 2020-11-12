c98d1acba33eb2eb36f9bded9ee1334cfd915f8c
Zhennong Chen <chenzhennong@gmail.com>
2020-11-12 13:19:53 -0500

    put tf back

---
+++ src/dvpy/tf/IteratorBase.py
@@ -0,0 +1,70 @@
+# System
+import threading
+
+# Third Party
+import numpy as np
+
+
+
+
+class IteratorBase(object):
+
+    __slots__ = [
+        "N",          
+        "batch_size",
+        "shuffle",
+        "batch_index",
+        "total_batches_seen",
+        "lock",
+        "index_generator",
+    ]
+
+    def __init__(self, N, batch_size, shuffle, seed):
+        self.N = N  # the number of total cases
+        self.batch_size = batch_size
+        self.shuffle = shuffle # whether the index_list is randomized
+
+        self.batch_index = 0 
+        self.total_batches_seen = 0 # count how many batches has been input
+        self.lock = threading.Lock()
+
+        self.index_generator = self._flow_index(N, batch_size, shuffle, seed)
+
+    def reset(self):
+        self.batch_index = 0
+
+    def _flow_index(self, N, batch_size=32, shuffle=False, seed=None):
+        # ensure self.batch_index is 0
+        self.reset()
+        while True:
+            if self.batch_index == 0:
+                index_array = np.arange(N)
+                if shuffle:   
+                    if seed is not None:
+                        np.random.seed(seed + self.total_batches_seen)
+                    index_array = np.random.permutation(N)
+
+            current_index = (self.batch_index * batch_size) % N
+            # Should this be >, rather than >=?
+            # https://github.com/tensorflow/tensorflow/blob/r1.2/tensorflow/contrib/keras/python/keras/preprocessing/image.py#L788
+            if N >= current_index + batch_size:   # the total number of cases is adequate for next loop
+                current_batch_size = batch_size
+                self.batch_index += 1
+            else:
+                current_batch_size = N - current_index  # not adequate, should reduce the batch size
+                self.batch_index = 0
+            self.total_batches_seen += 1
+            yield (
+                index_array[current_index : current_index + current_batch_size],
+                current_index,
+                current_batch_size,
+            )
+
+    def __iter__(self):
+        # needed if we want to do something like:
+        # for x, y in data_gen.flow(...):
+        return self
+
+    def __next__(self, *args, **kwargs):
+        # self.next must be defined in the child class.
+        return self.next(*args, **kwargs)
---
+++ src/dvpy/tf/PredictIterator.py
@@ -0,0 +1,127 @@
+# System
+
+# Third Party
+import numpy as np
+from keras import backend as K
+import sympy as sym
+
+# Internal
+import dvpy as dv
+from . import IteratorBase
+import os
+
+class PredictIterator(IteratorBase):
+    def __init__(
+        self,
+        X,
+        y,
+        image_data_generator,
+        batch_size=32,
+        view = None,
+        relabel_LVOT = None,
+        shuffle=False,
+        seed=None,
+        input_adapter=None,
+        output_adapter=None,
+        shape=None,
+        input_channels=None,
+        output_channels=None,
+        augment=False,
+        normalize=False,
+    ):
+
+        if K.image_dim_ordering() != "tf":
+            raise Exception("Only tensorflow backend is supported.")
+
+        # if len(X) != len(y):
+
+        #     raise Exception(
+        #         "X (images tensor) and y (labels) "
+        #         "should have the same length. "
+        #         "Found: X.shape = %s, y.shape = %s"
+        #         % (np.asarray(X).shape, np.asarray(y).shape)
+        #     )
+        self.X = X
+        self.y = y
+        self.image_data_generator = image_data_generator
+        self.view = view
+        self.relabel_LVOT = relabel_LVOT
+        self.input_adapter = input_adapter
+        self.output_adapter = output_adapter
+        self.shape = shape
+        self.input_channels = input_channels
+        self.output_channels = output_channels
+        self.augment = augment
+        self.normalize = normalize
+        super(PredictIterator, self).__init__(X.shape[0], batch_size, shuffle, seed)
+
+    def next(self):
+        # for python 2.x.
+        # Keeps under lock only the mechanism which advances
+        # the indexing of each batch
+        # see http://anandology.com/blog/using-iterators-and-generators/
+        with self.lock:
+            index_array, current_index, current_batch_size = next(self.index_generator)
+        # The transformation of images is not under thread lock so it can be done in parallel
+
+        ##
+        ## Allocate Memory
+        ##
+
+        # Input Image in range 0 to 1
+        batch_x = np.zeros(
+            tuple([current_batch_size]) + self.shape + tuple([self.input_channels])
+        )
+
+        # Ground Truth Segmentation
+        batch_y1= np.zeros(
+            tuple([current_batch_size]) + self.shape + tuple([self.output_channels])
+        )
+        batch_y2=np.zeros(tuple([current_batch_size])+(3,))
+        batch_y3=np.zeros(tuple([current_batch_size])+(3,))
+        batch_y4=np.zeros(tuple([current_batch_size])+(3,))
+     
+
+        ##
+        ## Deal with Actual Data
+        ##
+
+        # index_array is a randomly shuffled list of cases for this batch
+        for i, j in enumerate(index_array):
+
+            # Retrieve the path to the input image...
+            x = self.X[j]
+            # ...and convert the path to a raw (unnormalized) image.
+            if self.input_adapter is not None:
+                x = self.input_adapter(x)
+
+            # Normalize the *individual* images to zero mean and unit std
+            if self.normalize:
+                batch_x[i] = dv.normalize_image(x)
+            else:
+                batch_x[i] = x
+
+            #batch_y1[i] = label
+            #batch_y2[i] = t_c_n
+            #batch_y3[i] = x_n
+            #batch_y4[i] = y_n
+            
+        ##
+        ## Return
+        ##
+
+        inputs = {
+            name: layer
+            for name, layer in zip(
+                self.image_data_generator.input_layer_names, [batch_x]
+            )
+        }
+        outputs = {
+            name: layer
+            for name, layer in zip(
+                self.image_data_generator.output_layer_names, [batch_y1,batch_y2,batch_y3,batch_y4]
+            )
+        }
+        
+
+        return (inputs, outputs)
